d7643b4ab52956b7907cb2e097b088c56030054f
Jeremy Talen <jtalen@gmail.com>
2026-05-23 21:59:01 +0200

    Add ignored local secrets header example for ESP32 credentials

---
+++ src/Spa/secrets.example.h
@@ -0,0 +1,8 @@
+#pragma once
+
+const char* Myssid = "YourSSID";
+const char* Mypassword = "YourPassword";
+
+const char* MqttHost = "YourMQTT-Broker-IP";
+const char* MqttUser = "NameMQTTBroker";
+const char* MqttPassword = "PasswordMQTTBroker";
